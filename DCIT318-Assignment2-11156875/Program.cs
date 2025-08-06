4cf474f65d30253bf4b663c85393e5e65784151c
Eugenia Minnah <eaminnah@st.ug.edu.gh>
2025-08-06 13:38:14 +0000

    Created a main program to run all three OOP tasks

---
+++ DCIT318-Assignment2-11156875/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DCIT318_Assignment2_11156875;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Choose an application:");
+        Console.WriteLine("1. Inheritance and Method Overriding");
+        Console.WriteLine("2. Abstract Classes and Methods");
+        Console.WriteLine("3. Interfaces");
+        Console.Write("Enter your choice: ");
+
+        string? choice = Console.ReadLine();
+
+        if (choice == null)
+        {
+            Console.WriteLine("No input received. Exiting...");
+            return;
+        }
+    }
+}
