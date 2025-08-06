64a9cdb9cee090f4ab137fb3cf0469ab9815d4ca
Eugenia Minnah <eaminnah@st.ug.edu.gh>
2025-08-06 14:28:42 +0000

    Added Animal, Dog, and Cat classes to demonstrate inheritance and method overriding

---
+++ DCIT318-Assignment2-11156875/Inheritance and Method Overriding .cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCIT318_Assignment2_11156875
+{
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+}
--- DCIT318-Assignment2-11156875/Program.cs
+++ DCIT318-Assignment2-11156875/Program.cs
@@ -11,12 +11,28 @@ class Program
         Console.WriteLine("3. Interfaces");
         Console.Write("Enter your choice: ");
 
-        string? choice = Console.ReadLine();
+        string choice = Console.ReadLine();
 
-        if (choice == null)
+        if (string.IsNullOrWhiteSpace(choice))
         {
             Console.WriteLine("No input received. Exiting...");
             return;
+        }
+
+        switch (choice)
+        {
+            case "1":
+                Animal a = new Animal();
+                Animal d = new Dog();
+                Animal c = new Cat();
+                Console.WriteLine("\nAnimal:");
+                a.MakeSound();
+                Console.WriteLine("Dog:");
+                d.MakeSound();
+                Console.WriteLine("Cat:");
+                c.MakeSound();
+                break;
+
         }
     }
 }
